1c02a9e3cff04a774c648356b97cedb76da33aa1
nylee16 <nylee16@sookmyung.ac.kr>
2025-06-22 17:38:11 +0900

    화재 이미지 인식 관련 파일 수정 (by.nayeon 25.06.22)
    Update dataset_loader.py

--- 2414115/fire_image_detection모델학습 ver2/dataset_loader.py
+++ 2414115/fire_image_detection모델학습 ver2/dataset_loader.py
@@ -1,5 +1,5 @@
 from roboflow import Roboflow
-rf = Roboflow(api_key="zAkmnLnZgDoFFfBnKvKR")
+rf = Roboflow(api_key="") #key 입력 필요
 project = rf.workspace("smopensourcefiredetection").project("firedetection-9toil")
 version = project.version(7)
 dataset = version.download("yolov8")
